c1359ffd9449d27279c66de45614bb8ae8e3bcca
Sushant Sinha <sinhasushant04@gmail.com>
2021-05-12 10:08:49 +0530

    DB added

--- src/main/java/com/example/demo/dao/CustomerDAO.java
+++ src/main/java/com/example/demo/dao/CustomerDAO.java
@@ -4,7 +4,12 @@ import com.example.demo.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerDAO extends CrudRepository<Customer,Integer> {
+
+    @Override
+    List<Customer> findAll();
 
 }
--- src/main/java/com/example/demo/service/customerService.java
+++ src/main/java/com/example/demo/service/customerService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.CustomerDAO;
 import com.example.demo.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,35 +11,47 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Component
 public class customerService {
 
+    @Autowired
+    private CustomerDAO customerDAO;
+
     private int idCount=1;
     private List<Customer> customerList = new CopyOnWriteArrayList<>();
 
     public Customer addCustomer( Customer customer){
 
-        customer.setId(idCount);
+        /*customer.setId(idCount);
         customerList.add(customer);
-        idCount++;
-        return customer;
+        idCount++;*/
+
+        //return customer;
+
+        // no id is passed
+
+        return customerDAO.save(customer);
 
     }
 
     public List<Customer> getAllCustomer(){
 
-        return customerList;
+        //return customerList;
+
+        return customerDAO.findAll();
 
     }
 
     public Customer getCustomer(int id){
-        return customerList
+        /*return customerList
                 .stream()
                 .filter(c -> c.getId() == id)
                 .findFirst()
-                .get();
+                .get();*/
+
+        return customerDAO.findById(id).get();
     }
 
     public Customer updateCustomer(int id, Customer customer){
 
-        customerList
+        /*customerList
                 .stream()
                 .forEach(c ->{
                     if(c.getId() == id){
@@ -51,19 +65,27 @@ public class customerService {
                 .stream()
                 .filter(c -> c.getId() == customer.getId())
                 .findFirst()
-                .get();
+                .get();*/
+
+        // save when passed with id parameter works as update
+        // when no id is passed with the customer object, it works as creating new customer
+
+        customer.setId(id); // setting the id of the customer to be updated
+        return customerDAO.save(customer);
 
     }
 
     public void deleteCustomer(int id){
 
-        customerList
+        /*customerList
                 .stream()
                 .forEach(c ->{
                     if(c.getId() == id){
                         customerList.remove(c);
                     }
-                });
+                });*/
+
+        customerDAO.deleteById(id);
 
     }
 
